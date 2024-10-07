8877f41924271ba1d09a1eec484e04002d946331
Schania Karadaghi <schania_karadaghi@hotmail.com>
2024-10-07 13:23:17 +0200

    update tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('Front-end tests', () => {
     test('Test case 01', async ({ page }) => {
       // perform login
-      await page.goto(`${process.env.BASE_URL}`);
-      await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
+      await page.goto('http://localhost:3000');
       await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
       await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
       await page.getByRole('button', { name: 'Login' }).click();
