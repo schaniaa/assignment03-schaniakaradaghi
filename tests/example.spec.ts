e20735e539334fbc58f7562d4f4f7a2a54f7304e
Schania Karadaghi <schania_karadaghi@hotmail.com>
2024-10-07 13:20:20 +0200

    update tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,16 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Front-end tests', () => {
-  test('Create a client', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-    await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-    // Include the rest of the code...
-
+    test('Test case 01', async ({ page }) => {
+      // perform login
+      await page.goto(`${process.env.BASE_URL}`);
+      await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
+      await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+      await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+      await page.getByRole('button', { name: 'Login' }).click();
+      //await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+  
+  
+      // perform logout
+     await page.getByRole('button', { name: 'Logout' }).click();
+     await expect(page.url()).toBe('http://localhost:3000/login'); //assertion
+     await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
+    });
   });
-});
 
 test.describe('Backend tests', () => {
   test('Create a client', async ({ request }) => {
