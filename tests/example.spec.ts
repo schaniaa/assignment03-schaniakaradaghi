d53959432c8e5edab601e8b77f1d33a53074ef83
Schania Karadaghi <schania_karadaghi@hotmail.com>
2024-10-07 13:37:08 +0200

    update tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,36 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Front-end tests', () => {
-    test('Test case 01', async ({ page }) => {
-      // perform login
-      await page.goto('http://localhost:3000');
-      await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-      await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-      await page.getByRole('button', { name: 'Login' }).click();
-      //await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/login/');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
   
-  
-      // perform logout
-     await page.getByRole('button', { name: 'Logout' }).click();
-     await expect(page.url()).toBe('http://localhost:3000/login'); //assertion
-     await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
-    });
+    await page.locator('input[type="text"]').fill("tester01");
+    await page.locator('input[type="password"]').fill("GteteqbQQgSr88SwNExUQv2ydb7xuf8c");
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForTimeout(10000);
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview'})).toBeVisible();
   });
+    
+    test('Log in', async ({ page }) => {
+      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview'})).toBeVisible();
+    
+    });
+    
+    
+    test('Create client', async ({ page }) => {
+    
+      await page.locator('div').filter({ hasText: /^ClientsNumber: 2View$/ }).getByRole('link').click();
+    
+      await page.getByRole('link', { name: 'Create Client' }).click();
+    
+      await expect(page.getByRole('heading', { name: 'New Client'})).toBeVisible();
+      await page.fill('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]', "Lena Handen");
+      await page.fill('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=email]', "Lena@Handen.se");
+      await page.fill('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=text]', "12345678");
+      await page.getByText('Save').click()
+      await expect(page.getByRole('heading', { name: 'Lena Handen'})).toBeVisible();
+    });
 
 test.describe('Backend tests', () => {
   test('Create a client', async ({ request }) => {
@@ -28,5 +43,5 @@ test.describe('Backend tests', () => {
     expect (response.ok()).toBeTruthy();    
     // Include the rest of the code...
   });  
+  });  
 });
-
